cd6f7ff44f17e3d76dc80514ba15806c04e28555
hands-rec <hands.records@gmail.com>
2016-04-08 08:34:09 +0900

    Add NeoBundle

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# 
+# NeoBunele
+# https://github.com/Shougo/neobundle.vim
+# https://github.com/Shougo/neobundle.vim#if-you-want-to-install-manually-or-you-are-using-windows
+echo 'Install NeoBundle -------'
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+echo '-------------------------'
